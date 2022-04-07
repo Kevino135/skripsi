beb5540222a3c99d50d750a23b8b0fee955c8289
kevin <kevinnolasco>
2022-04-07 09:55:11 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import os
 import json
+from tabnanny import check
 import git
 
 
@@ -29,50 +30,85 @@ def isPassword(data):
         return "No Password Found"
     return clean_match
 
-
-def isCredentials(regex_api, data):
-    regex_private_key = regex_api["private_key"]
-    regex_api_creds = regex_api["other"]
-
+def isCredentials(regex_creds, read_file, read_file_lines):
+    count_issue = 1
     clean_match = dict()
-    for key_type, values in regex_private_key.items():
-        match = re.findall(values, data)
-        if len(match) != 0:
-            clean_match[key_type+" 1"] = match
 
-    for api_creds_type, values in regex_api_creds.items():
-        match = re.findall(values, data)
-        if len(match) != 0:
-            clean_match[api_creds_type] = match
+    # find creds with regex
+    for regex_type, values in regex_creds.items():
+        regex = re.compile(values)
+
+        for file, vals in read_file.items():
+            match = regex.findall(vals)
     
-    if len(clean_match) == 0:
-        return "No API Creds Found"
-    return clean_match
-    # HW => after each match, remove the string from data so it will not read the same string
+            # assign issue if exists
+            for m in match:
+                clean_match["issue " + str(count_issue)] = dict()
+                clean_match["issue " + str(count_issue)]["type"] = regex_type
+                clean_match["issue " + str(count_issue)]["match"] = m
+                clean_match["issue " + str(count_issue)]["file"] = file
+                
+                # get line number
+                if "private" in regex_type:
+                    header = m.split("\n")[1]
+                    first = (read_file_lines[file].index(header) + 1) - 1
+                    last = first + len(m.split("\n")) - 1
+                    clean_match["issue " + str(count_issue)]["line"] = f"{first} - {last}"
+                else:
+                    first = (read_file_lines[file].index(m) + 1) - 1
+                    clean_match["issue " + str(count_issue)]["line"] = str(first)
+
+                count_issue += 1
+
+    for k, v in clean_match.items():
+        print(k, v, "\n")
+    return clean_match, count_issue
 
 
-def getStageFile():
+def readModifiedFile(modified_files):
+    read_file_lines = dict()
+    read_file       = dict()
+
+    for file in modified_files:
+        with open(file) as f:
+            data = f.readlines()
+            data = [x.replace("\n", "").strip() for x in data]
+            # data = [x for x in data if x != '']
+            read_file_lines[file] = data
+    
+    for file in modified_files:
+        with open(file) as f:
+            datas = f.read()
+            read_file[file] = datas
+
+    return read_file_lines, read_file
+
+
+def getModifiedFile():
     repo = git.Repo()
-    diff_list = repo.head.commit.diff()
-    print(diff_list)
-
+    diff_list = repo.git.diff('HEAD', name_only=True)
+    
+    return diff_list.split('\n')
 
 
 def main():
-    stageFile = getStageFile()
-    with open("regex.json")                 as f: regex_api = json.load(f)
+    # list modified file from git diff
+    modified_files = getModifiedFile()
 
+    # get list of regex
+    with open("regex.json") as f: regex_creds = json.load(f)
 
+    # read file per line and whole file
+    read_file_lines, read_file = readModifiedFile(modified_files)
 
-    # with open("sampleFile/test.py")    as f: data = f.read()
-
-
+    print("[+] Api Creds Check:")
+    checkCredentials, count_issue = isCredentials(regex_creds, read_file, read_file_lines)
+    # print(checkCredentials)
     # print("[+] Password Check:")
     # print(isPassword(data))
     # print("\n")
 
-    # print("[+] Api Creds Check:")
-    # print(isCredentials(regex_api, data))
+
 
 if __name__ == '__main__':
     main()
