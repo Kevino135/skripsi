7e419b2ee2df78367699ffccdb444ab1041a686d
kevin <kevinnolasco>
2022-04-10 14:43:39 +0700

    update main.py coloring

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 
 import re
 import passwordmeter
+import platform
+import os
+from colorama import init, Fore, Back
 
 # wordlist = [
 #     "password",
@@ -13,8 +16,9 @@ import passwordmeter
 # ]
 
 # reg = ".*({})[\s=(:\'\"<>]*([a-zA-Z0-9_\!\#,@\/\\\:\;.\|\`\$=\+\-\*\^\?\&\~\%]*)[\')><\"]*".format("|".join(wordlist))
+reg = "(?:password|pass|passwd|pswrd|pwd|pw|PGPASSWORD).*['\">](.*?)['\"<]"
 
-# regex = re.compile(reg, re.IGNORECASE)
+regex = re.compile(reg, re.IGNORECASE)
 
 string_list = [
     'd3v3l0p70608',
@@ -23,14 +27,25 @@ string_list = [
     "password_1",
     '7erc2rrrS*2018"',
     'os.getenv("PASSWORD")'
+    'PASSWORD= d3v3l0p7000',
+    'pwd = Password("andra")',
+    "pass='cfu_lIaN8qLrcg_pssp_ZJZE8gTEEj_wap_UaJ3f7bMvY_pssp_gDJyQQIe7K_be'"
+    'pw := "7erc2rrr*18"',
+    'passwd = "GetDataByMemberId"',
+    'if password = true',
+    'if strcmp(password, "yes")',
+    'password = os.getenv("PASSWORD")'
 ]
 
 
-# for string in string_list:
-#     match = regex.findall(string)
-#     print(match[0][1])
+for string in string_list:
+    match = regex.findall(string)
+    print(match)
 
-for i in string_list:
-    meter = passwordmeter.Meter(settings=dict(factors='length,charmix'))
-    strength = meter.test(i)[0]
-    print(strength)
+# for i in string_list:
+#     meter = passwordmeter.Meter(settings=dict(factors='length,charmix'))
+#     strength = meter.test(i)[0]
+#     print(strength)
+
+init()
+print(Fore.CYAN + Back.LIGHTBLACK_EX + "ANDRA")
