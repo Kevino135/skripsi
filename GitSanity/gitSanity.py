2e046b9affdf78bcd70186310152e6fc7298eb5c
Glenn <54360473+Kurohayabusa-r@users.noreply.github.com>
2022-04-21 22:14:41 +0700

    Update gitSanity.py

--- GitSanity/gitSanity.py
+++ GitSanity/gitSanity.py
@@ -1,14 +1,12 @@
 import re
 import os
 import json
-import magic
 import passwordmeter
 import platform
 import shutil
 from datetime import datetime
 import pkg_resources
 from cryptography.fernet import Fernet
-from colorama import init, Fore, Back, Style
 
 def main():
     print("Hello World")
