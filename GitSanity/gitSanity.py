2fb675491bca92348d1cf4282d3f19784749d274
kevin <kevinnolasco>
2022-04-25 14:36:54 +0700

    add display for list issue & details, minus commit

--- GitSanity/gitSanity.py
+++ GitSanity/gitSanity.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
 from functools import partial
+from lib2to3.pgen2.token import LEFTSHIFT
 import re
 import os
 import json
+from typing import final
 import magic
 import passwordmeter
 import platform
 import sys
 import shutil
-from GitSanity.compression import *
+from compression import *
 from datetime import datetime
 import pkg_resources
 from tkinter import *
 from tkinter import ttk
 from cryptography.fernet import Fernet
 from colorama import init, Fore, Back, Style
+
 
 def getPasswordComplexity(password):
     meter = passwordmeter.Meter(settings=dict(factors="length,charmix"))
@@ -331,6 +334,92 @@ def commitAction():
 
 
 def showTkinterWindow(final_res):
+    class ScrollbarFrame(Frame):
+        """
+        Extends class tk.Frame to support a scrollable Frame 
+        This class is independent from the widgets to be scrolled and 
+        can be used to replace a standard tk.Frame
+        """
+        def __init__(self, parent, width, height, window_x, window_y, **kwargs):
+            Frame.__init__(self, parent, **kwargs)
+            self.canvas_width  = width
+            self.canvas_height = height
+            self.window        = (window_x, window_y)
+
+            # The Scrollbar, layout to the right
+            vsb = Scrollbar(self, orient="vertical")
+            vsb.pack(side="right", fill="y")
+
+            # The Canvas which supports the Scrollbar Interface, layout to the left
+            self.canvas = Canvas(self, borderwidth=2, background="#ffffff", relief="groove",
+                                    width=self.canvas_width, height=self.canvas_height
+                                )
+            self.canvas.pack(side="left", fill="both", expand=True)
+            self.canvas.pack_propagate(0)
+
+            # Bind the Scrollbar to the self.canvas Scrollbar Interface
+            self.canvas.configure(yscrollcommand=vsb.set)
+            vsb.configure(command=self.canvas.yview)
+
+            # The Frame to be scrolled, layout into the canvas
+            # All widgets to be scrolled have to use this Frame as parent
+            self.scrolled_frame = Frame(self.canvas, background=self.canvas.cget('bg'))
+            self.canvas.create_window((self.window), window=self.scrolled_frame, anchor="nw")
+
+            # Configures the scrollregion of the Canvas dynamically
+            self.scrolled_frame.bind("<Configure>", self.on_configure)
+
+        def on_configure(self, event):
+            """Set the scroll region to encompass the scrolled frame"""
+            self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+ 
+
+    def initIssueDetails():
+        global details_v_issue, details_v_type, details_v_file, details_v_line, details_v_match
+        issue = "issue 1"
+
+        details_v_issue = ttk.Label(details_content_a_val_content, text=f"  {issue.split()[-1]}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_issue.pack(pady=4)
+        details_v_type = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['type']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_type.pack(pady=4)
+        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['file']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_file.pack(pady=4)
+        details_v_line = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['line']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_line.pack(pady=4)
+        details_v_match = ttk.Label(sbf_details.scrolled_frame, text=f"{final_res[issue]['match']}", 
+                                font=('Helvetica', 10), background="white", width=80, anchor='w')
+        details_v_match.pack(pady=2, padx=6)
+
+
+    def updateIssueDetails(issue):
+        global details_v_issue, details_v_type, details_v_file, details_v_line, details_v_match
+        details_v_issue.pack_forget()
+        details_v_type.pack_forget()
+        details_v_file.pack_forget()
+        details_v_line.pack_forget()
+        details_v_match.pack_forget()
+
+        details_v_issue = ttk.Label(details_content_a_val_content, text=f"  {issue.split()[-1]}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_issue.pack(pady=4)
+        details_v_type = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['type']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_type.pack(pady=4)
+        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['file']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_file.pack(pady=4)
+        details_v_line = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['line']}", 
+                                font=('Helvetica', 10), background="white", width=300, anchor='w')
+        details_v_line.pack(pady=4)
+        details_v_match = ttk.Label(sbf_details.scrolled_frame, text=f"{final_res[issue]['match']}", 
+                                font=('Helvetica', 10), background="white", width=80, anchor='w')
+        details_v_match.pack(pady=1, padx=6)
+
+
     key_label = list()
     issue_frame = list()
 
@@ -428,37 +517,188 @@ def showTkinterWindow(final_res):
 
 
     root = Tk()
-    root.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(int(-1*(event.delta/120)), "units"))
+    # root.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(int(-1*(event.delta/120)), "units"))
     root.title("GitSanity")
+    root.resizable(False, False)
 
-    # Create Container
-    container = ttk.Frame(root)
-    container.pack(fill="both", expand=True)
+    # Define Width & Height of Tools
+    tools_width = 800
+    tools_height = 650
+
+    # Get Screen Width & Height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Get Screen Center Position
+    center_width = int(screen_width/2) - int(tools_width/2)
+    center_height = int(screen_height/2) - int(tools_height/2)
+
+    root.geometry(f"{tools_width}x{tools_height}+{center_width}+{center_height}")
+
+    # Create Canvas for Root
+    root_container = ttk.Frame(root, borderwidth=1, relief='groove',
+                            width=tools_width, height=tools_height)
+    root_container.pack(fill='both', expand=True)
+
+    # Create Container Issue
+    issue_container = Frame(root_container, bd=1, relief='groove',
+                                width=200, height=tools_height,
+                                # padding='1 1 1 1'
+                                )
+    issue_container.pack(side=LEFT, fill='both')
+
+    # Create Container Issue - Header Canvas
+    issue_header = Canvas(issue_container, bg="white", width=170, height=25, bd=2)
+    issue_header.pack()
+
+    # Create Container Issue - Header Label
+    issue_header_text = ttk.Label(issue_header, text="Issue", 
+                                foreground="white", background="#555d50",
+                                width=21, 
+                                borderwidth=10, relief='groove',
+                                font=("Helvetica", 11),
+                                padding='5 5 9 5',
+                                anchor=CENTER
+                                )
+    issue_header_text.pack()
+
+    sbf_issue = ScrollbarFrame(issue_container, 150, 476, 11, 4)
+    sbf_issue.pack()
+
+    for i in range(len(final_res)):
+        Button(sbf_issue.scrolled_frame, text=f"Issue {str(i+1)} \u003E",
+                bd=0.5, font=('Helvetica', 9, ''),
+                width=18, activebackground="#adadad",
+                command=lambda i = i: updateIssueDetails(f"issue {str(i+1)}")
+              ).pack(pady=3)
+
+    # Create Container Issue - Total
+    t_issue = len(final_res)
+    issue_total = ttk.Label(issue_container, text=f"Total Issue: {str(t_issue)}",
+                        font=('', 9),
+                        width=23,
+                        anchor=W
+                        )
+    issue_total.pack()
+
+    # Create Container Details & Commit
+    details_commit_container = ttk.Frame(root_container, borderwidth=1, relief='groove',
+                                width=630, height=400,
+                                padding='1 1 1 1'
+                                )
+    details_commit_container.pack(side=LEFT, fill='both')
+
+    # Create Container Details - Header Canvas
+    details_header = Canvas(details_commit_container, bg="white", width=630, height=25, bd=2)
+    details_header.pack_propagate()
+    details_header.pack(side=TOP)
+
+    # Create Container Details - Header Label
+    details_header_text = ttk.Label(details_header, text="Details", 
+                                        foreground="white", background="#555d50",
+                                        width=74, 
+                                        borderwidth=10, relief='groove',
+                                        font=("Helvetica", 11),
+                                        padding='5 5 5 5',
+                                        anchor=CENTER
+                                        )
+    details_header_text.pack()
+
+    # Create Container Details - Content Canvas A
+    details_content_a = Canvas(details_commit_container, width=630, height=150, bd=1, relief="flat")
+    details_content_a.pack(fill='both')
+
+    # Create Container Details - Content Canvas A - Frame Key
+    details_content_a_key_frame = ttk.Frame(details_content_a, width=200, height=145)
+    details_content_a_key_frame.pack(fill=BOTH, side=LEFT)
+
+    # Create Container Details - Content Canvas A - Frame Key - Canvas & Label
+    details_content_a_key_canvas = Canvas(details_content_a_key_frame, width=90, height=140)
+    details_content_a_key_canvas.pack_propagate(0)
+    details_content_a_key_canvas.pack(side=BOTTOM)
+
+    for label in ("Issue", "Type", "File", "Line", "Match"):
+        ttk.Label(details_content_a_key_canvas, text=label, font=('Helvetica', 10),
+                  width=10, anchor='w').pack(pady=4)
+
+    # Create Container Details - Content Canvas A - Frame Value
+    details_content_a_val_frame = ttk.Frame(details_content_a, width=430, height=145)
+    details_content_a_val_frame.pack(fill=BOTH)
+
+    # Create Container Details - Content Canvas A - Frame Value - Canvas
+    details_content_a_val_canvas = Canvas(details_content_a_val_frame, bg="white", bd=2, relief='groove', width=430, height=145)
+    details_content_a_val_canvas.pack_propagate(0)
+    details_content_a_val_canvas.pack()
+
+    # Create Container Details - Content Canvas A - Frame Value - Canvas - Content
+    global details_content_a_val_content
+    details_content_a_val_content = Canvas(details_content_a_val_canvas, bg="white", highlightthickness=0, width=420, height=135)
+    details_content_a_val_content.pack_propagate(0)
+    details_content_a_val_content.pack(side=BOTTOM, padx=5, pady=8)
+
+    # Create Container Details - Content Canvas B - Content
+    global sbf_details
+    sbf_details = ScrollbarFrame(details_commit_container, 630, 250, 11, 4)
+    sbf_details.pack()
+
+    # Init Details Content
+    initIssueDetails()
+
+    # Create Container Commit
+    commit_container = ttk.Frame(details_commit_container, borderwidth=1, relief='groove',
+                                width=630, height=200,
+                                padding='1 1 1 1'
+                                )
+    commit_container.pack_propagate(0)
+    commit_container.pack(side=BOTTOM, fill='both')
+
+    # # Create Container Commit - Header Canvas
+    commit_header = Canvas(commit_container, bg="white", width=630, height=25, bd=2)
+    commit_header.pack()
+
+    # Create Container Commit - Header Label
+    commit_header_text = ttk.Label(commit_header, text="Commit Action", 
+                                        foreground="white", background="#555d50",
+                                        width=74, 
+                                        borderwidth=10, relief='groove',
+                                        font=("Helvetica", 11),
+                                        padding='5 5 5 5',
+                                        anchor=CENTER
+                                        )
+    commit_header_text.pack()
+
+    # Create Container Commit - Content Canvas
+    commit_content = Canvas(commit_container, width=630, height=180, bd=1, relief="groove")
+    commit_content.pack_propagate(0)
+    commit_content.pack()
+
+    # Lanjut dari sini ...
+
 
     # Create Canvas
-    canvas = Canvas(container, height=500, width=450)
-    canvas.pack(side="left", fill="both", expand=True)
+    # canvas = Canvas(container, height=500, width=450, background="blue")
+    # canvas.pack(side="left", fill="both", expand=True)
 
     # Add Scrollbar
-    scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
-    scrollbar.pack(side=RIGHT, fill=Y)
+    # scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
+    # scrollbar.pack(side=RIGHT, fill=Y)
 
     # Configure Canvas
-    canvas.configure(yscrollcommand=scrollbar.set)
-    mainframe = ttk.Frame(canvas, padding="20")
-    mainframe.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+    # canvas.configure(yscrollcommand=scrollbar.set)
+    # mainframe = ttk.Frame(canvas, padding="20")
+    # mainframe.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
 
     # Add Frame to a Window in the Canvas
-    canvas.create_window((0, 0), window=mainframe, anchor="nw")
+    # canvas.create_window((0, 0), window=mainframe, anchor="nw")
     
-    resultFrame(mainframe)
+    # resultFrame(mainframe)
 
-    btn_encrypt = Button(mainframe, text="Continue with encryption", command=continueWithEncryption)
-    btn_encrypt.grid(column=0, row=1)
-    btn_continue = Button(mainframe, text="Continue without encryption", command=continueWithoutEncryption)
-    btn_continue.grid(column=0, row=2, pady=10)
-    btn_cancel = Button(mainframe, text="Cancel", command=closeRootWithExit1)
-    btn_cancel.grid(column=0, row=3)
+    # btn_encrypt = Button(mainframe, text="Continue with encryption", command=continueWithEncryption)
+    # btn_encrypt.grid(column=0, row=1)
+    # btn_continue = Button(mainframe, text="Continue without encryption", command=continueWithoutEncryption)
+    # btn_continue.grid(column=0, row=2, pady=10)
+    # btn_cancel = Button(mainframe, text="Cancel", command=closeRootWithExit1)
+    # btn_cancel.grid(column=0, row=3)
 
     root.mainloop()
 
@@ -535,7 +775,8 @@ def main():
 
     # remove extraction path
     shutil.rmtree(extraction_path)
-    return exit_code
+    # return exit_code
+    return 1
 
 if __name__ == "__main__":
     # initialize colorama init
