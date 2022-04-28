76db5e71f467e02a63cbea22b3e8fcfc401bcbee
Glenn <54360473+Kurohayabusa-r@users.noreply.github.com>
2022-04-29 00:00:43 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="GitSanity",
-    version="0.1.7",
+    version="0.1.9",
     url="https://github.com/Kevino135/skripsi",
     author="Kevin Nolasco, William Suryajaya, Glenn Yohanes",
     author_email="",
     description="Detects potential credential in code with option to encrypt",
     long_description=open('README.md').read(),
     packages=setuptools.find_packages(),
-    install_requires=['colorama==0.4.4', 'cryptography==36.0.2', 'passwordmeter==0.1.8', 'py7zr==0.18.4', 'python-magic==0.4.15', 'python-magic-bin==0.4.14'],
+    install_requires=['cryptography==36.0.2', 'passwordmeter==0.1.8'],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
