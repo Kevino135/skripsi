9d33b2f47ef956e9aa4f9fa1f90dc9a6aa85f179
Glenn <54360473+Kurohayabusa-r@users.noreply.github.com>
2022-04-28 23:57:41 +0700

    Add files via upload

--- GitSanity/gitSanity.py
+++ GitSanity/gitSanity.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 
-from functools import partial
 import re
 import os
 import json
-import magic
 import passwordmeter
-import platform
-import shutil
-from GitSanity.compression import *
-from datetime import datetime
 import pkg_resources
 from tkinter import *
 from tkinter import ttk
 from cryptography.fernet import Fernet
-from colorama import init, Fore, Back, Style
+
 
 
 def getPasswordComplexity(password):
@@ -138,7 +132,7 @@ def readModifiedFile(modified_files):
     return read_file_lines, read_file
 
 
-def getModifiedFile(extraction_path):
+def getModifiedFile():
     # get staged file/folder
     diff_list = os.popen("git diff --name-only --cached").read()
 
@@ -146,36 +140,6 @@ def getModifiedFile(extraction_path):
         return None, None
 
     all_files = diff_list.strip().split("\n")
-
-    # extraction
-    compressed_file = dict()
-    for files in all_files:
-        file_type = magic.from_file(files, mime=True)      
-        if file_type in compression_type.keys():
-            if file_type == "application/x-gzip":
-                tgz = (".tar.gz", ".txt.gz", ".tgz")
-                gz  = (".gz")
-
-                if files.endswith(tgz):
-                    decompress = compression_type[file_type]["tgz"]
-                elif files.endswith(gz):
-                    decompress = compression_type[file_type]["gz"]
-
-            else:
-                decompress = compression_type[file_type]
-
-            decompress.decompress(files, extraction_path)
-
-            list_decompress = [extraction_path + f"/{x}" for x in decompress.list(files)]
-            for f in list_decompress:
-                if os.path.isfile(f):
-                    compressed_file[f] = files
-
-    # remove compressed file and add its extraction to list
-    for key, vals in compressed_file.items():
-        if vals in all_files:
-            all_files.remove(vals)
-        all_files.append(key)
 
     file_ignore = (
         ".jpg", ".jpeg", ".png", ".gif", ".svg",
@@ -189,148 +153,10 @@ def getModifiedFile(extraction_path):
         if not files.endswith(file_ignore):
             filtered_file.append(files)
     
-    return filtered_file, compressed_file
+    return filtered_file
 
 
-def selectFilesToEncrypt(final_res):
-    files_to_encrypt = dict()
-    print("")
-    issue_input = input("Enter issues number to encrypt (ex: 1,2,5): ")
-    issue_input = issue_input.split(",")
-    for inputs in issue_input:
-        if "issue {}".format(inputs) in final_res.keys():
-            file_name = final_res["issue {}".format(inputs)]["file"]
-            if file_name not in files_to_encrypt:
-                files_to_encrypt[file_name] = dict()
-            files_to_encrypt[file_name]["issue {}".format(inputs)] = dict()
-            files_to_encrypt[file_name]["issue {}".format(inputs)]["match"] = final_res["issue {}".format(inputs)]["match"]
-            files_to_encrypt[file_name]["issue {}".format(inputs)]["line"] = final_res["issue {}".format(inputs)]["line"]
-
-    return files_to_encrypt
-
-
-def getCompressedPath(compressed_file):
-    compressed_path = dict()
-
-    for key, vals in compressed_file.items():
-        if vals not in compressed_path.keys():
-            compressed_path[vals] = list()
-        
-        split_file_path = key.split("/")
-        get_parent = "/".join(split_file_path[:2])
-
-        if get_parent not in compressed_path[vals]:
-            compressed_path[vals].append(get_parent)
-
-    return compressed_path
-
-
-def doEncrypt(files_to_encrypt):
-    key_input = -1
-    while key_input < 0 or key_input > 2:
-        print("")
-        print("1. Generate new key")
-        print("2. Use existing generated key")
-        print("0. Cancel")
-        while True:
-            try:
-                key_input = int(input("Select [1/2/0]: "))
-                break
-            except ValueError:
-                print("Enter an integer")
-    if key_input == 1:
-        enc_key = Fernet.generate_key()
-        print("")
-        print("Key: {}\n(Save this key, it is used for encryption and decryption)".format(enc_key.decode()))
-        fernet = Fernet(enc_key)
-        for file_name, issues in files_to_encrypt.items():
-            with open(file_name, "r") as f:
-                lines = f.read()
-                for issue in issues:
-                    encrypted_string = fernet.encrypt(issues[issue]["match"].encode())
-                    lines = lines.replace(issues[issue]["match"], encrypted_string.decode())
-            with open(file_name, "w") as f:
-                f.write(lines)
-
-    elif key_input == 2:
-        print("")
-        enc_key = input("Enter key: ")
-        fernet = Fernet(enc_key)
-        for file_name, issues in files_to_encrypt.items():
-            with open(file_name, "r") as f:
-                lines = f.read()
-                for issue in issues:
-                    encrypted_string = fernet.encrypt(issues[issue]["match"].encode())
-                    lines = lines.replace(issues[issue]["match"], encrypted_string.decode())
-            with open(file_name, "w") as f:
-                f.write(lines)
-
-    elif key_input == 0:
-        pass
-
-    return 1 
-
-
-def doCompress(compressed_path, extraction_path):
-    for target_path, extract_path in compressed_path.items():
-        file_type = magic.from_file(target_path, mime=True)      
-        if file_type in compression_type.keys():
-            if file_type == "application/x-gzip":
-                tgz = (".tar.gz", ".txt.gz", ".tgz")
-                gz  = (".gz")
-
-                if target_path.endswith(tgz):
-                    compress = compression_type[file_type]["tgz"]
-                elif target_path.endswith(gz):
-                    compress = compression_type[file_type]["gz"]
-
-            else:
-                compress = compression_type[file_type]
-
-            compress.compress(target_path, extract_path, extraction_path)
-
-
-def printOut(final_res, compressed_file, extraction_path):
-    os.system("cls" if platform.system().lower() == "windows" else "clear")
-
-    header = " SCAN RESULT "
-
-    print("\n" + Back.RED + Style.BRIGHT + header)
-    print("=" * len(header), "\n")
-
-    for issue, details in final_res.items():
-        print(Fore.LIGHTBLUE_EX + issue.capitalize())
-
-        for key, values in details.items():
-            if key == "match":
-                print("%-12s : %s%-2s" % (key.capitalize(), Fore.LIGHTGREEN_EX, values))
-            elif key == "file":
-                if values in compressed_file:
-                    file_path = values.replace(extraction_path+"/", compressed_file[values] + " -> ")
-                    print("%-12s : %-2s" % (key.capitalize(), file_path))
-                else:
-                    print("%-12s : %-2s" % (key.capitalize(), values))
-            else:
-                print("%-12s : %-2s" % (key.capitalize(), values))
-        print("\n")
-    
-
-def commitAction():
-    # header
-    print("-"*45)
-    print("| " + Back.MAGENTA + " "*13 + " COMMIT ACTION " + " "*13 + Back.RESET + " |")
-    print("-"*45)
-    
-    print("|" + " "*2 + "1." + " "*2 + "|" + " " + "Continue with encryption"       + " "*11 + "|")
-    print("|" + " "*2 + "2." + " "*2 + "|" + " " + "Continue without encryption"    + " "*8  + "|")
-    print("|" + " "*2 + "0." + " "*2 + "|" + " " + "Cancel"                         + " "*29 + "|")
-
-    # footer
-    print("|" + " "*6 + "|" + " "*36 + "|")
-    print("-"*45)
-
-
-def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_file):
+def showTkinterWindow(final_res):
     class ScrollbarFrame(Frame):
         """
         Extends class tk.Frame to support a scrollable Frame 
@@ -374,6 +200,17 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
 
             # Configures the scrollregion of the Canvas dynamically
             self.scrolled_frame.bind("<Configure>", self.on_configure)
+            self.scrolled_frame.bind("<Enter>", self._bound_to_mousewheel)
+            self.scrolled_frame.bind("<Leave>", self._unbound_to_mousewheel)
+        
+        def _bound_to_mousewheel(self, event):
+            self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+        def _unbound_to_mousewheel(self, event):
+            self.canvas.unbind_all("<MouseWheel>")
+
+        def _on_mousewheel(self, event):
+            self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
         def on_configure(self, event):
             """Set the scroll region to encompass the scrolled frame"""
@@ -390,14 +227,7 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
         details_v_type = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['type']}", 
                                 font=('Helvetica', 10), background="white", width=300, anchor='w')
         details_v_type.pack(pady=4)
-
-        paths = final_res[issue]["file"]
-        if extraction_path in paths:
-            issue_path = paths.replace(extraction_path+"/", compressed_file[paths] + " -> ").replace(extraction_path+"\\", compressed_file[paths] + " -> ")            
-        else:
-            issue_path = paths
-
-        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {issue_path}", 
+        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['file']}", 
                                 font=('Helvetica', 10), background="white", width=300, anchor='w')
         details_v_file.pack(pady=4)
         details_v_line = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['line']}", 
@@ -422,14 +252,7 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
         details_v_type = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['type']}", 
                                 font=('Helvetica', 10), background="white", width=300, anchor='w')
         details_v_type.pack(pady=4)
-
-        paths = final_res[issue]["file"]
-        if extraction_path in paths:
-            issue_path = paths.replace(extraction_path+"/", compressed_file[paths] + " -> ").replace(extraction_path+"\\", compressed_file[paths] + " -> ")            
-        else:
-            issue_path = paths
-
-        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {issue_path}", 
+        details_v_file = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['file']}", 
                                 font=('Helvetica', 10), background="white", width=300, anchor='w')
         details_v_file.pack(pady=4)
         details_v_line = ttk.Label(details_content_a_val_content, text=f"  {final_res[issue]['line']}", 
@@ -438,30 +261,6 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
         details_v_match = ttk.Label(sbf_details.scrolled_frame, text=f"{final_res[issue]['match']}", 
                                 font=('Helvetica', 10), background="white", width=80, anchor='w')
         details_v_match.pack(pady=1, padx=6)
-
-
-    key_label = list()
-    issue_frame = list()
-
-    def resultFrame(mainframe):
-        result_frame = ttk.Frame(mainframe)
-        result_frame.grid(column=0, row=0)
-        lbl = ttk.Label(result_frame, text="Scan Result")
-        lbl.grid(column=0, row=0)
-
-        issue_count = 1
-        for issue, details in final_res.items():
-            issue_frame.append(LabelFrame(result_frame, text=issue.capitalize()))
-            issue_frame[-1].grid(column=0, row=issue_count, pady=10)
-
-            detail_count = 1
-            for key, values in details.items():
-                key_label.append(ttk.Label(issue_frame[-1], text="{} : {}".format(key.capitalize(), values)))
-                key_label[-1].grid(column=0, row=detail_count, sticky=W)
-
-                detail_count += 1
-            
-            issue_count += 1
 
 
     def continueWithEncryption():
@@ -518,16 +317,13 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
             files_content_frame = ttk.Frame(files_content, width=200, height=100)
             files_content_frame.pack(fill=BOTH, pady=4, padx=4)
 
-            sbf_files = ScrollbarFrame(files_content_frame, 200, 100, 0, 0, False)
+            sbf_files = ScrollbarFrame(files_content_frame, 200, 100, 0, 0, horizontal=True)
             sbf_files.pack()
             sbf_files.scrolled_frame.grid_columnconfigure(0, minsize=200)
+
             i = 0
             for file_name, issues in files_to_encrypt.items():
-                if extraction_path in file_name:
-                    paths = compressed_file[file_name]
-                else:
-                    paths = file_name
-                Label(sbf_files.scrolled_frame, text=paths).grid(column=0, row=i, pady=2)
+                Label(sbf_files.scrolled_frame, text=file_name).grid(column=0, row=i, pady=2)
                 i += 1
             # for i in range(10):
             #     Label(sbf_files.scrolled_frame, text=f"File {i}.txt").grid(column=0, row=i, pady=2)
@@ -535,24 +331,6 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
             Button(popupWindow, text="Finish", width=30, 
                 font=('Helvetica', 9, ''), background="#ccc", 
                 command=closeRootWithExit1).grid(column=0, row=2, pady=10)
-
-        def doCompress():
-            for target_path, extract_path in compressed_path.items():
-                file_type = magic.from_file(target_path, mime=True)      
-                if file_type in compression_type.keys():
-                    if file_type == "application/x-gzip":
-                        tgz = (".tar.gz", ".txt.gz", ".tgz")
-                        gz  = (".gz")
-
-                        if target_path.endswith(tgz):
-                            compress = compression_type[file_type]["tgz"]
-                        elif target_path.endswith(gz):
-                            compress = compression_type[file_type]["gz"]
-
-                    else:
-                        compress = compression_type[file_type]
-
-                    compress.compress(target_path, extract_path, extraction_path)
 
 
         selectIssueWindow = Toplevel(root)
@@ -582,7 +360,7 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
                 command=generateKey).grid(column=0, row=4, pady=10)
         Button(selectIssueWindow, text="Continue", width=40, 
                 font=('Helvetica', 9, ''), background="#ccc", 
-                command=lambda: [filesToEncrypt(), doCompress()]).grid(column=0, row=5)
+                command=filesToEncrypt).grid(column=0, row=5)
         Button(selectIssueWindow, text="Cancel", width=40, 
                 font=('Helvetica', 9, ''), background="#ccc", 
                 command=selectIssueWindow.destroy).grid(column=0, row=6, pady=10)
@@ -811,47 +589,14 @@ def showTkinterWindow(final_res, compressed_path, extraction_path, compressed_fi
                             command=closeRootWithExit1)
     btn_cancel.grid(column=0, row=3, pady=10)
 
-
-    # Create Canvas
-    # canvas = Canvas(container, height=500, width=450, background="blue")
-    # canvas.pack(side="left", fill="both", expand=True)
-
-    # Add Scrollbar
-    # scrollbar = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
-    # scrollbar.pack(side=RIGHT, fill=Y)
-
-    # Configure Canvas
-    # canvas.configure(yscrollcommand=scrollbar.set)
-    # mainframe = ttk.Frame(canvas, padding="20")
-    # mainframe.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-
-    # Add Frame to a Window in the Canvas
-    # canvas.create_window((0, 0), window=mainframe, anchor="nw")
-    
-    # resultFrame(mainframe)
-
-    # btn_encrypt = Button(mainframe, text="Continue with encryption", command=continueWithEncryption)
-    # btn_encrypt.grid(column=0, row=1)
-    # btn_continue = Button(mainframe, text="Continue without encryption", command=continueWithoutEncryption)
-    # btn_continue.grid(column=0, row=2, pady=10)
-    # btn_cancel = Button(mainframe, text="Cancel", command=closeRootWithExit1)
-    # btn_cancel.grid(column=0, row=3)
-
     root.mainloop()
 
 
 def main():
     global exit_code
-    # create folder to store extraction
-    time_data = datetime.now()
-    fmt_date = "%Y%m%d%H%M%S"
-
-    extraction_path = datetime.strftime(time_data, fmt_date)
-    if not os.path.exists(extraction_path):
-        os.mkdir(extraction_path)
 
     # list modified file from git diff
-    modified_files, compressed_file = getModifiedFile(extraction_path)
+    modified_files = getModifiedFile()
     
     if modified_files is None:
         print("\nIt seems there is no file/directories in staged\n")
@@ -877,48 +622,13 @@ def main():
 
         # commit action
         if final_res:
-            compressed_path  = getCompressedPath(compressed_file)
-            showTkinterWindow(final_res, compressed_path, extraction_path, compressed_file)
-            # printOut(final_res, compressed_file, extraction_path)
-            # continue_input = -1
-            # while continue_input < 0 or continue_input > 2:
-            #     print("")
-            #     commitAction()
-            #     while True:
-            #         try:
-            #             continue_input = int(input("Select [1/2/0]: "))
-            #             break
-            #         except ValueError:
-            #             print("Enter an integer")
-
-            # if continue_input == 1:
-            #     files_to_encrypt = selectFilesToEncrypt(final_res)
-            #     compressed_path  = getCompressedPath(compressed_file)
-                
-            #     exit_code = doEncrypt(files_to_encrypt)
-            #     doCompress(compressed_path, extraction_path)
-
-            # elif continue_input == 2:
-            #     confirm = ""
-            #     while confirm.lower() != "y" and confirm.lower() != "n":
-            #         confirm = input("Confirm to continue without encryption (Y/n): ")
-            #         if confirm.lower() == "y":
-            #             exit_code = 0
-            #         elif confirm.lower() == "n":
-            #             exit_code = 1
-            # elif continue_input == 0:
-            #     exit_code = 1
+            showTkinterWindow(final_res)
         else:
-            print("\nNo hardcoded credentials detected in your repository\n")
             exit_code = 0
 
-    # remove extraction path
-    shutil.rmtree(extraction_path)
     return exit_code
 
 if __name__ == "__main__":
-    # initialize colorama init
-    init(autoreset=True)
     # exit code for scan result
     exit_code = 0
     main()
